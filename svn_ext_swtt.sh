38732c5a838aa8d7f76b3bafb3a6503e1dec24ac
trodevel <trodevel@gmail.com>
2017-12-05 02:12:34 +0100

    added support of many branches

--- svn_ext_swtt.sh
+++ svn_ext_swtt.sh
@@ -20,15 +20,20 @@
 
 # SKV 17516
 
-VER="1.0"
+# 1.0 - 17516 - initial commit
+# 1.1 - 17b04 - added support of many branches
+
+VER="1.1"
 
 #<hb>#############################################################################
 #
-# switch branch to trunk in SVN externals
+# switch branches to trunk in SVN externals
 #
-# ./svn_ext_swtt.sh <branch_top_name> <revision_in_trunk> <folder>
+# ./svn_ext_swtt.sh <branch_top_name>[,<branch_top_name_2>[,...]]  <revision_in_trunk> <folder>
 #
 # Example: ./svn_ext_swtt.sh some_lib 123456 externals
+#
+#          ./svn_ext_swtt.sh some_lib,another_lib 123456 externals
 #
 #<he>#############################################################################
 
@@ -37,26 +42,40 @@ show_help()
     sed -e '1,/^#<hb>/d' -e '/^#<he>/,$d' $0 | cut -b3-    #"
 }
 
-BRANCH_TOP=$1
+switch_branch()
+{
+    local BRANCH_TOP=$1
+    local EXT_FILE=$2
+    local REV=$3
+
+    local old_name=$( grep $BRANCH_TOP $EXT_FILE )
+
+    sed -i "s~\s*\(.*\)\(/branches\)\(.*/$BRANCH_TOP\)\(/.*\) \(.*\)~-r $REV \1/trunk\3    \5~" $EXT_FILE
+
+    local new_name=$( grep $BRANCH_TOP $EXT_FILE )
+
+    echo "old name = $old_name"
+    echo "new name = $new_name"
+}
+
+BRANCHES_TOP_CSV=$1
 REV=$2
 EXT_PATH=$3
 
-[[ -z "$BRANCH_TOP" ]] && { echo; echo "ERROR: BRANCH_TOP is not defined"; show_help; exit; }
-[[ -z "$REV" ]]        && { echo; echo "ERROR: REV is not defined"; show_help; exit; }
-[[ -z "$EXT_PATH" ]]   && { echo; echo "ERROR: EXT_PATH is not defined"; show_help; exit; }
+[[ -z "$BRANCHES_TOP_CSV" ]] && { echo; echo "ERROR: BRANCHES_TOP is not defined"; show_help; exit; }
+[[ -z "$REV" ]]              && { echo; echo "ERROR: REV is not defined"; show_help; exit; }
+[[ -z "$EXT_PATH" ]]         && { echo; echo "ERROR: EXT_PATH is not defined"; show_help; exit; }
 
 EXT_FILE=svn_${RANDOM}.txt
 
 svn propget svn:externals $EXT_PATH > $EXT_FILE
 
-old_name=$( grep $BRANCH_TOP $EXT_FILE )
+BRANCHES_TOP=$( echo $BRANCHES_TOP_CSV | tr ',' ' ' )
 
-sed -i "s~\s*\(.*\)\(/branches\)\(.*/$BRANCH_TOP\)\(/.*\) \(.*\)~-r $REV \1/trunk\3    \5~" $EXT_FILE
-
-new_name=$( grep $BRANCH_TOP $EXT_FILE )
-
-echo "old name = $old_name"
-echo "new name = $new_name"
+for s in $BRANCHES_TOP
+do
+    switch_branch $s "$EXT_FILE" $REV
+done
 
 svn propset svn:externals $EXT_PATH -F $EXT_FILE
 
